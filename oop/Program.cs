4a981aba87cdb8ff790b718ca6e85c5a0f50b945
Ben <benjamin.welland@student.manchester.ac.uk>
2024-01-04 14:42:49 +0000

    Add project files.

---
+++ oop/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace oop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class and create an object
+            Calculator calc = new Calculator();
+            // Create a local variable to hold the answer
+            int x = 3;
+            int y = 5;
+            int sum = 0;
+            sum = calc.Add(x, y);
+            Console.WriteLine("sum of {0} + {1} = {2}", x, y, sum);
+            Console.ReadLine();
+        }
+    }
+}
